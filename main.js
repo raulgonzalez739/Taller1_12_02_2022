d2aa925ce59d3af73702cf7757f56891378723fb
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-14 21:03:15 -0500

    todo_Sobre_Variables_14_02_2022

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-alert('Hola mundo punto 3, Raul');
-console.log('Hola mundo punto 3, Raul');
+alert('Punto 4, Raul');
+console.log('Punto 4, Todo sobre variables, Raul');
+let variablestring = 'texto de prueba';
+console.log(variablestring);
+let variablenumero = 7;
+console.log(variablenumero);
+let variableboolenanosi = true;
+console.log(variableboolenanosi);
